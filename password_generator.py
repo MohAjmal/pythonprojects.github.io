0e242ce4c12aa15541349664f9187c9bb7d995e3
MohAjmal <86249080+MohAjmal@users.noreply.github.com>
2024-10-03 19:26:24 +0530

    Update password_generator.py
    Modified password_generator. It now generates passwords containing Letters, Symbols, and Numbers in no particular order, making it more random.

--- password_generator.py
+++ password_generator.py
@@ -5,16 +5,25 @@ numbers = ['0', '1', '2', '3', '4', '5', '6', '7', '8', '9']
 symbols = ['!', '#', '$', '%', '&', '(', ')', '*', '+']
 
 print("Welcome to the PyPassword Generator!")
-nr_letters= int(input("How many letters would you like in your password?\n")) 
+nr_letters= int(input("How many letters would you like in your password?\n"))
 nr_symbols = int(input(f"How many symbols would you like?\n"))
 nr_numbers = int(input(f"How many numbers would you like?\n"))
-
-for i in range(0,nr_letters):
-  random_letter = random.randint(0,len(letters)-1)
-  print(letters[random_letter],end="")
-for i in range(0,nr_symbols):
-  random_symbol = random.randint(0,len(symbols)-1)
-  print(symbols[random_symbol],end="")
-for i in range(0,nr_numbers):
-  random_number = random.randint(0,len(numbers)-1)
-  print(numbers[random_number],end="")
+tot=nr_letters+nr_numbers+nr_symbols
+letcount, numcount, symcount =0,0,0
+while tot>0:
+    rando=random.randint(1,3)
+    if rando==1 and letcount<nr_letters:
+        random_letter = random.randint(0,len(letters)-1)
+        print(letters[random_letter],end="")
+        letcount+=1
+        tot-=1
+    if rando==2 and symcount<nr_symbols:
+        random_symbol = random.randint(0,len(symbols)-1)
+        print(symbols[random_symbol],end="")
+        symcount+=1
+        tot-=1
+    if rando==3 and numcount<nr_numbers:
+        random_number = random.randint(0,len(numbers)-1)
+        print(numbers[random_number],end="")
+        numcount+=1
+        tot-=1
